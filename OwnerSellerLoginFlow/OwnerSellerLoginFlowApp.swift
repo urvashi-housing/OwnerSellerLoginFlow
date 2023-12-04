b48d8ab6da406aa1172040f9a8c62316a1acf678
Urvashi <urvashi.gupta@housing.com>
2023-12-04 14:13:49 +0530

    Initial Commit

---
+++ OwnerSellerLoginFlow/OwnerSellerLoginFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnerSellerLoginFlowApp.swift
+//  OwnerSellerLoginFlow
+//
+//  Created by Urvashi Gupta on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct OwnerSellerLoginFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
